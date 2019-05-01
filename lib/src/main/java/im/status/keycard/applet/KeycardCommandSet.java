3ae2324b08c041be30886b9fc2e1b04c2573d750
Alex Miller <asmiller1989@gmail.com>
2019-05-01 17:31:54 -0500

    Removes logs and byte switch

--- lib/src/main/java/im/status/keycard/applet/KeycardCommandSet.java
+++ lib/src/main/java/im/status/keycard/applet/KeycardCommandSet.java
@@ -173,9 +173,9 @@ public class KeycardCommandSet {
    * @throws IOException communication error
    */
   public APDUResponse select(int instanceIdx) throws IOException {
-    APDUCommand selectApplet = new APDUCommand(0x00, 0xA4, 0x20, 0, Identifiers.getKeycardInstanceAID(instanceIdx));
+    APDUCommand selectApplet = new APDUCommand(0x00, 0xA4, 4, 0, Identifiers.getKeycardInstanceAID(instanceIdx));
     APDUResponse resp =  apduChannel.send(selectApplet);
-
+  
 
     if (resp.getSw() == 0x9000) {
       info = new ApplicationInfo(resp.getData());
